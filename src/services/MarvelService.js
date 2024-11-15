056057bb13d22816ca5adef7f90cddf552067b74
Artem <art0m1krth@gmail.com>
2024-11-15 19:56:00 +0300

    v 1.0

---
+++ src/services/MarvelService.js
@@ -0,0 +1,18 @@
+
+class MarvelService{
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=01943cb588b09f2fc9e5f2cd8cebb6f5'); 
+    }
+}
+
+export default MarvelService;
